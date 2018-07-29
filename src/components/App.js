a1ae64710e1a2ff96fcc3f4447bab569f8f2f5e8
Alex J. Gatz <the.neocoder@gmail.com>
2018-07-29 00:13:39 -0400

    Moved many things
    First I moved the entire menu component into app.js and changed the app function into a react class to make it easier for me (a react noob) to use state to change "left-margin" to match the distance the sid nav pops out.

--- src/components/App.js
+++ src/components/App.js
@@ -1,71 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import Menu from "./Menu";
 import Main from "./Main";
-
-const sideNavBar = {
-  height: "100%",
-  width: "0",
-  position: "fixed",
-  top: "0",
-  left: "0",
-  overflowX: "hidden",
-  transition: "0.5s"
-};
-
-const sideNavBarOpen = {
-  height: "100%",
-  width: "150px",
-  position: "fixed",
-  zIndex: "1",
-  top: "0",
-  left: "0",
-  backgroundColor: "#111",
-  opacity: "0.7",
-  overflowX: "hidden",
-  transition: "0.5s",
-  paddingTop: "60px"
-};
-
-const closeButton = {
-  position: "absolute",
-  top: "0",
-  right: "25px",
-  fontSize: "36px",
-  marginLeft: "50px",
-  cursor: "pointer",
-  color: "#f2f2f2"
-};
-
-const sideNavLinks = {
-  padding: "8px 8px 8px 32px",
-  textDecoration: "none",
-  fontSize: "25px",
-  color: "#818181",
-  display: "block",
-  transition: "0.3s"
-};
-
-const hamburgerButton = {
-  transition: "all 1.6s",
-  fontSize: "30px",
-  cursor: "pointer"
-};
-
-const hamburgerButtonFaded = {
-  transition: "all 0.2s",
-  opacity: "0"
-};
-
-const marginLeft = {
-  transition: "all 0.8s",
-  marginLeft: "0px"
-};
-
-const marginMoved = {
-  transition: "all 0.2s",
-  marginLeft: "150px"
-};
+import "./App.css";
 
 class App extends Component {
   constructor() {
@@ -77,45 +13,50 @@ class App extends Component {
     this.setState({ open: !this.state.open });
   }
   render() {
-    let sideNavStyle = this.state.open ? sideNavBar : sideNavBarOpen;
+    /* Is there a better way to do this?!?! */
+
+    let sideNavStyle = this.state.open ? "sideNavBar" : "sideNavBarOpen";
     let hamburgerButtonStyle = this.state.open
-      ? hamburgerButton
-      : hamburgerButtonFaded;
-    let marginStyle = this.state.open ? marginLeft : marginMoved;
+      ? "hamburgerButton"
+      : "hamburgerButtonFaded";
+    let marginStyle = this.state.open ? "marginLeft" : "marginMoved";
 
     return (
-      <div>
-        <div style={sideNavStyle}>
-          <a style={closeButton} onClick={this.openNav.bind(this)}>
+      <body>
+        <div className={sideNavStyle}>
+          <a className="closeButton" onClick={this.openNav.bind(this)}>
             &times;
           </a>
           <a>
-            <Link style={sideNavLinks} to="/">
+            <Link className="sideNavLinks" to="/">
               Home
             </Link>
           </a>
           <a>
-            <Link style={sideNavLinks} to="/about">
+            <Link className="sideNavLinks" to="/about">
               About
             </Link>
           </a>
           <a>
-            <Link style={sideNavLinks} to="/contact">
+            <Link className="sideNavLinks" to="/contact">
               Contact
             </Link>
           </a>
         </div>
 
         <div>
-          <span style={hamburgerButtonStyle} onClick={this.openNav.bind(this)}>
+          <span
+            className={hamburgerButtonStyle}
+            onClick={this.openNav.bind(this)}
+          >
             &#9776;
           </span>
           {/* Added to allow the nav menu to cause the leftmargin to move when the nav bar does*/}
-          <div style={marginStyle}>
+          <div className={marginStyle}>
             <Main />
           </div>
         </div>
-      </div>
+      </body>
     );
   }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,10 @@
 import React from "react";
+import "./Home.css";
 
-const Home = () => <div />;
+const Home = () => (
+  <div>
+    <h1 className="homeTitle">I'm Alex Gatz</h1>
+  </div>
+);
 
 export default Home;
