ffdb151abb913aafe70af50b2f741ed50b38197e
Alex J. Gatz <the.neocoder@gmail.com>
2018-08-13 23:11:14 -0400

    img added to Home.js
    img added to home to play with using it as a background image. Works well so i'm going to use that same method for about and contact. Currently trying to locate some SVG files to use as a background so I can add some interesting effects or animations if I am able.

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,10 @@ import "./Home.css";
 
 const Home = () => (
   <div className="home">
+    <img
+      className="home-background"
+      src="https://uploads.codesandbox.io/uploads/user/d9d5cefe-e7a3-4c81-9c8a-bbf46cd15d18/NoUC-pexels-photo-276092.jpeg"
+    />
     <h1 className="homeTitle">Hello World</h1>
   </div>
 );
