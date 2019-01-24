53b1364606ee9b66b9dfafc444b0ac8c9443ae81
Alex J. Gatz <the.neocoder@gmail.com>
2019-01-24 01:10:51 -0500

    Big Changed
    Changed colors, background, added the particle effect i've been wanting forever now. Just randomly realized exactly how to implement it. <3 React js for that intuitive nature.

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/App";
-import { ParallaxProvider } from "react-scroll-parallax";
+import Particles from "react-particles-js";
 
 render(
-  <ParallaxProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ParallaxProvider>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById("root")
 );
