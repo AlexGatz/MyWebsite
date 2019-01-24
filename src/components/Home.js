2fb4989fe58ed44ccb700a7d7d828ed1d7eaab59
Alex J. Gatz <the.neocoder@gmail.com>
2019-01-23 23:59:35 -0500

    I'm back!
    Back to web dev. Thought I would like java, hate java moving back to the world of Web Dev!

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ const Home = () => (
       className="home-background"
       src="https://uploads.codesandbox.io/uploads/user/d9d5cefe-e7a3-4c81-9c8a-bbf46cd15d18/NoUC-pexels-photo-276092.jpeg"
     />
-    <h1 className="homeTitle">Hello World</h1>
+    <h1 className="homeTitle">Alex Gatz</h1>
   </div>
 );
 
